038665771f9c5101211d242b5a607ae262f3e107
NickMonks <nicolasmg94@gmail.com>
2021-12-17 15:02:56 +0000

    Continue

---
+++ clipstash/src/lib/domain/clip/field/clip_id.rs
@@ -0,0 +1,24 @@
+use derive_more::Constructor;
+use serde::{Serialize,Deserialize};
+use crate::data::DbId;
+
+#[derive(Clone, Debug, Constructor, Serialize, Deserialize)]
+pub struct ClipId(DbId);
+
+impl ClipId {
+    pub fn into_inner(self) -> DbId {
+        self.0
+    }
+}
+
+impl From<DbId> for ClipId {
+    fn from(id: DbId) -> Self {
+        Self(id)
+    }
+}
+
+impl Default for ClipId {
+    fn default() -> Self {
+        Self(DbId::nil())
+    }
+}
